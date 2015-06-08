9772ad2eefc7717aadc8c63a7bb563082d9a4232
Carl Swanson <cetswanson@gmail.com>
2015-06-08 11:44:14 -0400

    Add random line method to quotes rb

--- quotes.rb
+++ quotes.rb
@@ -3,8 +3,15 @@ require 'rack/server'
 
 class Quote
   def response
-    [200, {}, 'Test']
   end
+end
+
+def pick_random_line(file_name)
+  chosen_line = nil
+  File.foreach("fixtures/" + file_name).each_with_index do |line, number|
+    chosen_line = line if rand < 1.0/(number+1)
+  end
+  return chosen_line
 end
 
 class QuotesApp
