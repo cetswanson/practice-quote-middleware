415470688154c34a7d8b652be8fe5d83d706b1db
Carl Swanson <cetswanson@gmail.com>
2015-06-10 02:03:32 -0400

    Add quote to text file comparison spec

--- spec/quotes_spec.rb
+++ spec/quotes_spec.rb
@@ -7,6 +7,12 @@ describe Quote do
     expect(quote_app.pick_random_line("rickygervais.txt")).to be_a String
   end
 
+  it "quote should return a quote from txt file" do
+    full_file = read_file("./fixtures/rickygervais.txt")
+    test_quote = quote_app.pick_random_line("rickygervais.txt")
+    expect((full_file).include?(test_quote)).to be true
+  end
+
   def read_file( file_name )
     File.read( file_name )
   end
