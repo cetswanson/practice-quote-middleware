6ab2ba067ee32d8af703400c4a9a21670de8af1f
Carl Swanson <cetswanson@gmail.com>
2015-06-10 02:05:28 -0400

    Add read file method to quotes spec

--- spec/quotes_spec.rb
+++ spec/quotes_spec.rb
@@ -6,4 +6,7 @@ describe Quote do
   it "quote should return a string" do
     expect(quote_app.pick_random_line("rickygervais.txt")).to be_a String
   end
+  def read_file( file_name )
+    File.read( file_name )
+  end
 end
