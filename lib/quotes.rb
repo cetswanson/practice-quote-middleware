1aa73f4a6aedc9056e4f8c43981cd1aa3e4da9b5
Carl Swanson <cetswanson@gmail.com>
2015-06-09 22:35:13 -0400

    Add response method to quote class

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -10,4 +10,9 @@ class Quote
   def call(env)
     [status, headers, response]
   end
+
+  def response
+    [200, {"Content-Type" => "text/plain"}, pick_random_line("rickygervais.txt")]
+  end
+
 end
