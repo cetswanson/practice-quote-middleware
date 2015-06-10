4174f150a92deeb9b21bbc8c59351e29a627dd0b
Carl Swanson <cetswanson@gmail.com>
2015-06-09 23:42:55 -0400

    Refactor quote class

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -3,15 +3,7 @@ require 'rack/server'
 
 class Quote
 
-  def initialize(app)
-    @app = app
-  end
-
   def call(env)
-    [status, headers, response]
-  end
-
-  def response
     [200, {"Content-Type" => "text/plain"}, [pick_random_line("rickygervais.txt")]]
   end
 
