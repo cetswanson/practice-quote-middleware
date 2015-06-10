a974ab3e2268422c2c61ac7179bff076cf47b2d1
Carl Swanson <cetswanson@gmail.com>
2015-06-09 22:35:41 -0400

    Add pick random line method to to quote class

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -15,4 +15,12 @@ class Quote
     [200, {"Content-Type" => "text/plain"}, pick_random_line("rickygervais.txt")]
   end
 
+  def pick_random_line(file_name)
+    chosen_line = nil
+    File.foreach("fixtures/" + file_name).each_with_index do |line, number|
+      chosen_line = line if rand < 1.0/(number+1)
+    end
+    return chosen_line
+  end
+
 end
